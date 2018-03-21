ab8019e0c1749f08ade4290e80a4a9cdef1e4d90
mgan85 <mgan@o2.pl>
2018-03-21 19:28:58 +0000

    task 3

--- basics-assignment-3-start/basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .afterFour {
+      background: blue;
+    }
+  `]
 })
 export class AppComponent {
+  Logs = [];
+  counter = 1;
+
+  addLog() {
+    this.Logs.push(this.counter);
+    this.counter++;
+  }
+
+  showParagraph() {
+    return this.counter % 2 === 0;
+  }
+
+  getColor(log) {
+    return log > 4 ? "white" : "black";
+  }
 }
