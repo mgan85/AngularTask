af746fea8a9603f4aaec447551fe4594081ac5df
mgan85 <mgan@o2.pl>
2018-03-21 20:25:41 +0000

    task 1

--- basics-assignment-1-start/basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-warning-alert',
   templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  styles: [`
+    .warning {
+      background-color: red;
+      color: black;
+      border: 2px solid darkred;
+      width: 10em;
+      margin:auto;
+    }
+  `]
 })
 export class WarningAlertComponent {
 
